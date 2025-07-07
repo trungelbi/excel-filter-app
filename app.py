0e97e30a137bea9198d67a3b595802ac3a574c38
Trung NB <trung31891@gmail.com>
2025-07-07 19:36:19 +0700

    pu

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ st.markdown("<div style='margin-top: 20px;'></div>", unsafe_allow_html=True)
 # Tải kết quả về CSV
 csv = filtered_df.to_csv(index=False).encode("utf-8-sig")
 
-st.download_button("📥 Tải kết quả CSV", csv, "ketqua.csv", "text/csv")
+st.download_button("📥 Kết xuất toàn bộ dữ liệu", csv, "ketqua.csv", "text/csv")
